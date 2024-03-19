269663bf5f76152ef5c43785943861499c107f05
CarlosGiovaneNN <carlosgiovane1289@gmail.com>
2024-03-19 17:48:28 -0300

    where tf is the css

--- scripts/src.js
+++ scripts/src.js
@@ -1,34 +1,94 @@
-const min = 1, max = 8;
+const inputs = document.querySelectorAll("input");
+const min = 1;
+let max = 10;
+let partision = 10;
 
-const getValues = (n) => {
-    const interval = max - min;
-    const variation = interval / n;
+const resSpan = document.getElementById("res");
 
-    const arr = [];
+const verifyInputValue = (id, value) => {
+  const currentInput = document.getElementById(id);
 
-    for(let i = 0; i < n; i++){
-        const currentValue = i > 0 ? arr[i-1][1] : min;
-        const lastValue = i === n-1 ? max :  currentValue + variation
-        arr.push([currentValue, lastValue]);
-    }
-    
-    const y = []
-    const rectangles = []
-    
-    arr.forEach(item => {
-        const avg = (item[0] + item[1])/2;
-        const height = Math.log10(avg**3);
-        
-        rectangles.push((item[1] - item[0])*height)
-        y.push(height)
-    })
+  switch (id) {
+    case "input__partition":
+      if (value) {
+        value = Number(value);
+        if (value < 1) {
+          partision = 10;
+          currentInput.value = partision;
+        } else partision = value;
+      } else partision = 10;
 
-    const totalArea = rectangles.reduce((acc, i) => acc + i, 0);
+      break;
+    case "input__max":
+      if (value) {
+        value = Number(value);
+        if (value < 2) {
+          max = 10;
+          currentInput.value = max;
+        } else max = value;
+      } else max = 10;
 
-    return {
-        intervals: arr,
-        y,
-        rectangles,
-        totalArea
-    }
-}
+      break;
+  }
+
+  getValues();
+};
+
+let currentTimeouts = {};
+
+const handleInputChange = (e) => {
+  const { target } = e;
+  const { value, id } = target;
+
+  if (currentTimeouts?.[id]) {
+    clearTimeout(currentTimeouts?.[id]);
+  }
+
+  const timeout = setTimeout(() => {
+    verifyInputValue(id, value);
+  }, 600);
+
+  currentTimeouts = { ...currentTimeouts, [id]: timeout };
+};
+
+inputs.forEach((i) => {
+  i.addEventListener("keyup", handleInputChange);
+  i.addEventListener("change", handleInputChange);
+});
+
+const getValues = () => {
+  const interval = max - min;
+  const variation = interval / partision;
+
+  const arr = [];
+
+  for (let i = 0; i < partision; i++) {
+    const currentValue = i > 0 ? arr[i - 1][1] : min;
+    const lastValue = i === partision - 1 ? max : currentValue + variation;
+    arr.push([currentValue, lastValue]);
+  }
+
+  const y = [];
+  const rectangles = [];
+
+  arr.forEach((item) => {
+    const avg = (item[0] + item[1]) / 2;
+    const height = Math.log10(avg ** 3);
+
+    rectangles.push((item[1] - item[0]) * height);
+    y.push(height);
+  });
+
+  const totalArea = rectangles.reduce((acc, i) => acc + i, 0);
+
+  const resObj = {
+    intervals: arr,
+    y,
+    rectangles,
+    totalArea,
+  };
+
+  resSpan.innerText = `${totalArea.toFixed(6)} u²`;
+};
+
+document.addEventListener("DOMContentLoaded", getValues);
